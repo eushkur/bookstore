a9f995cbd400fa7f48429ce71f30fc7624aafd2c
EvgenyUshkur <110987191+eushkur@users.noreply.github.com>
2023-03-21 23:51:50 +0300

    add useParams + useNavigate

--- src/pages/DetailsPage/DetailsPage.tsx
+++ src/pages/DetailsPage/DetailsPage.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 
 export const DetailsPage = () => {
   const { name } = useParams();
+  const navigate = useNavigate()
+
+  const handleBack = () => {
+    navigate(-1);
+  };
 
   return (
      <div>
+         <button className="btn btn-primary" onClick={handleBack}>Back</button>
          <h1>{name}</h1>
      </div>
   );
