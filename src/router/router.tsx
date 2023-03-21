021cc9fbabcc8b2cfd91cc3ce732ff0845b6b405
EvgenyUshkur <110987191+eushkur@users.noreply.github.com>
2023-03-21 22:20:53 +0300

    add RequareAuth

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,15 +1,16 @@
 import { createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
-import { MainPage } from '../pages/MainPage';
-import { AccountPage } from '../pages/AccountPage';
-import { BookPage } from '../pages/BookPage';
-import { CartPage } from '../pages/CartPage';
-import { FavoritesPage } from '../pages/FavoritesPage';
-import { SearchPage } from '../pages/SearchPage';
-import { SignInPage } from '../pages/SignInPage';
+import { MainPage } from '../pages/MainPage/MainPage';
+import { AccountPage } from '../pages/AccountPage/AccountPage';
+import { BookPage } from '../pages/BookPage/BookPage';
+import { CartPage } from '../pages/CartPage/CartPage';
+import { FavoritesPage } from '../pages/FavoritesPage/FavoritesPage';
+import { SearchPage } from '../pages/SearchPage/SearchPage';
+import { SignInPage } from '../pages/SignInPage/SignInPage';
 import { MainTemplate } from '../templates/MainTemplate';
 import { ROUTE } from './routes';
-import { SignUpPage } from '../pages/SignUpPage';
-import { ResetPage } from '../pages/ResetPage';
+import { SignUpPage } from '../pages/SignUpPage/SignUpPage';
+import { ResetPage } from '../pages/ResetPage/ResetPage';
+import { RequareAuth } from '../components/molecules/RequareAuth/RequareAuth';
 
 export const router = createBrowserRouter(
     createRoutesFromElements(
@@ -18,11 +19,13 @@ export const router = createBrowserRouter(
             <Route path={ROUTE.ACCOUNT} element={<AccountPage />} />
             <Route path={ROUTE.BOOK} element={<BookPage />} />
             <Route path={ROUTE.CART} element={<CartPage />} />
-            <Route path={ROUTE.FAVORITES} element={<FavoritesPage />} />
             <Route path={ROUTE.SEARCH} element={<SearchPage />} />
             <Route path={ROUTE.SIGN_IN} element={<SignInPage />} />
             <Route path={ROUTE.SIGN_UP} element={<SignUpPage />} />
             <Route path={ROUTE.RESET} element={<ResetPage />} />
+            <Route element={<RequareAuth />}>
+                <Route path={ROUTE.FAVORITES} element={<FavoritesPage />} />
+            </Route>  
         </Route>
   
     )
