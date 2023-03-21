84192d0bee6580cbe716e866de468768abd2a1bb
EvgenyUshkur <110987191+eushkur@users.noreply.github.com>
2023-03-21 23:24:55 +0300

    add generate path in main page

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { generatePath, Link } from 'react-router-dom';
+import { ROUTE } from '../../router/routes';
 
 export const MainPage = () => {
+  const countries = [
+    {id: 1, name: 'John' },
+    {id: 2, name: 'Nill' },  
+  ]
   return (
     <div>
         <h1>MAINPAGE</h1>
+        {countries.map(country => {
+          return (
+            <Link to={generatePath(ROUTE.DETAILS, {id: country.name })} >
+              {country.name}
+            </Link >
+          );  
+        })}
     </div>
   );
 };
