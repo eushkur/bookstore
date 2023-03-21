fb962d6f8822e13ea31748d742c05866d793b724
EvgenyUshkur <110987191+eushkur@users.noreply.github.com>
2023-03-21 23:38:12 +0300

    removed detailed page from navigation

--- src/pages/DetailsPage/DetailsPage.tsx
+++ src/pages/DetailsPage/DetailsPage.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 export const DetailsPage = () => {
+  const { name } = useParams();
+
   return (
      <div>
-         <h1>Details</h1>
+         <h1>{name}</h1>
      </div>
-  )
-}
+  );
+};
