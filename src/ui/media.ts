ab7f67a01c295bdd574f0a33ba2b98a0bcb2c4b3
EvgenyUshkur <110987191+eushkur@users.noreply.github.com>
2023-03-24 17:27:16 +0300

    add colors, media, typography

---
+++ src/ui/media.ts
@@ -0,0 +1,9 @@
+export enum Breakpoint {
+    LG = 1280,
+    MD = 768,
+  }
+  
+  export const MediaQuery = {
+    LG: `@media screen and (max-width: ${Breakpoint.LG}px)`,
+    MD: `@media screen and (max-width: ${Breakpoint.MD}px)`,
+  };
---
+++ src/ui/typography.ts
@@ -0,0 +1,49 @@
+import { css } from "styled-components";
+import { MediaQuery } from "./media";
+
+
+const H1 = css`
+  font-weight: 700;
+  font-size: 56px;
+  line-height: 64px;
+  ${MediaQuery.MD} {
+    font-size: 32px;
+    line-height: 44px;
+  }
+`;
+
+const H2 = css`
+  font-weight: 700;
+  font-size: 40px;
+  line-height: 60px;
+  ${MediaQuery.MD} {
+    font-size: 28px;
+    line-height: 40px;
+  }
+`;
+
+const H3 = css`
+  font-weight: 700;
+  font-size: 24px;
+  line-height: 32px;
+`;
+
+const S1 = css`
+  font-weight: 700;
+  font-size: 18px;
+  line-height: 24px;
+`;
+
+const BODY = css`
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 32px;
+`;
+
+const BODY2 = css`
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 24px;
+`;
+
+export { H1, H2, H3, S1,BODY, BODY2 };
