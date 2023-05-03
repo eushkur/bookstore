2d4dd5e7029f1fd840b60726f1437cf968719042
EvgenyUshkur <110987191+eushkur@users.noreply.github.com>
2023-05-03 14:59:01 +0300

    barrel pattern for component

--- src/components/atoms/Form1/Form1.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-interface FormProps {
-  heading: string;
-}
-export const Form1 = ({ heading }: FormProps) => {
-  return <form>{heading}</form>;
-};
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,23 +1,61 @@
 import { Button } from "./atoms/Button/Button";
-import { Form1 } from "./atoms/Form1/Form1";
 import { Input } from "./atoms/Input/Input";
 import { Title } from "./atoms/Title/Title";
 import { BookCard } from "./molecules/BookCard/BookCard";
 import { RequareAuth } from "./molecules/RequareAuth/RequareAuth";
-import { CountryList } from "./organisms/CountryList/CountryList";
 import { Footer } from "./organisms/Footer/Footer";
 import { RegistrationCustomLink } from "./molecules/RegistrationCustomLink/RegistrationCustomLink";
 import { HeaderLink } from "./molecules/HeaderLink/HeaderLink";
+import { ButtonLike } from "./atoms/ButtonLike/ButtonLike";
+import { CloseCard } from "./atoms/CloseCard/CloseCard";
+import { SecondaryTitle } from "./atoms/SecondaryTitle/SecondaryTitle";
+import { Slider } from "./atoms/Slider/Slider";
+import { TabBar } from "./atoms/TabBar/TabBar";
+import { CartCard } from "./molecules/CartCard/CartCard";
+import { FavoritesBooks } from "./molecules/FavoritesBooks/FavoritesBooks";
+import { FavoritesCard } from "./molecules/FavoritesCard/FavoritesCard";
+import { FreeBooks } from "./molecules/FreeBooks/FreeBooks";
+import { Portal } from "./molecules/Portal/Portal";
+import { SearchHeader } from "./molecules/SearchHeader/SearchHeader";
+import { SignInForm } from "./molecules/SignInForm/SignInForm";
+import { SignUpForm } from "./molecules/SignUpForm/SignUpForm";
+import { Header } from "./organisms/Header/Header";
+import { Registration } from "./organisms/Registration/Registration";
+import { SimilarBooks } from "./organisms/SimilarBooks/SimilarBooks";
+import { BurgerMenu } from "./atoms/Burger/Burger";
+import { Error } from "./atoms/Error/Error";
+import { Notification } from "./molecules/Notification/Notification";
+import { Subscription } from "./molecules/Subscription/Subscription";
+import { DetailsBook } from "./organisms/DetailBook/DetailsBook";
 
 export {
+  BurgerMenu,
   Button,
-  Form1,
+  ButtonLike,
+  CloseCard,
+  Error,
   Input,
+  SecondaryTitle,
+  Slider,
+  TabBar,
   Title,
   BookCard,
+  CartCard,
+  FavoritesBooks,
+  FavoritesCard,
+  FreeBooks,
   HeaderLink,
-  RequareAuth,
-  CountryList,
-  Footer,
+  Notification,
+  Portal,
   RegistrationCustomLink,
+  RequareAuth,
+  SearchHeader,
+  SignInForm,
+  SignUpForm,
+  Subscription,
+  DetailsBook,
+  Footer,
+  Header,
+  Registration,
+  SimilarBooks,
 };
